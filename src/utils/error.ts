bc9388af64c51286d091f6b04d0b280afa80f04b
AlfieriChou <alfierichou@gmail.com>
2019-03-06 10:28:52 +0800

    grpc error handle

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -2,5 +2,10 @@ import { ServiceError as grpcServiceError, status } from 'grpc'
 
 export class ServiceError implements grpcServiceError {
   public name: string = 'ServiceError'
-  constructor(public code: status, public message: string) {}
+  public code: status
+  public message: string
+  constructor(code: status, message?: string) {
+    this.code = code
+    this.message = message || 'Service Error'
+  }
 }
