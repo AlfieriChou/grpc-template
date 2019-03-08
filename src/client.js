5ae89261394965c342800afda558ada2b469fb50
AlfieriChou <alfierichou@gmail.com>
2019-03-08 10:09:55 +0800

    js client demo

---
+++ src/client.js
@@ -0,0 +1,20 @@
+const messageGrpc = require('../model/message_grpc_pb')
+const message = require('../model/message_pb')
+const grpc = require('grpc')
+
+const client = new messageGrpc.MessageServiceClient(
+  '0.0.0.0:7000',
+  grpc.credentials.createInsecure()
+)
+
+const request = new message.MessageRequest()
+request.setMessageId('dis')
+request.setMessageType('what')
+
+const metadata = new grpc.Metadata()
+
+client.addMessage(request, metadata, (err, data) => {
+  if (err) throw err
+  const result = data.toObject()
+  console.log('[gRPC]: ', result)
+})
